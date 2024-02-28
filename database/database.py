b36b532484bd3a7c8c10c2e3197e1e91b4b68da8
hpHammer <hp.hammer.80@gmail.com>
2024-02-28 10:28:55 +0100

    Apply fix for threading error

--- database/database.py
+++ database/database.py
@@ -1,7 +1,9 @@
 import sqlite3
+import threading
 
 conn = sqlite3.connect('vrchess.db', check_same_thread=False)
 cursor = conn.cursor()
+lock = threading.Lock()
 
 # Create tables if they don't exist
 def initialize_database():
@@ -16,28 +18,44 @@ def initialize_database():
         )
     ''')
 
+def queryLock(func):
+    def wrapper(*args, **kwargs):
+        try:
+            lock.acquire(True)
+            return func(*args, **kwargs)
+        finally:
+            lock.release()
+    return wrapper
+
+def execute_query(query, params):
+    cursor.execute(query, params)
+    conn.commit()
+
 # Get user by id
+@queryLock
 def get_user_by_id(id):
-    cursor.execute('SELECT * FROM users WHERE id = ?', (id,))
+    execute_query('SELECT * FROM users WHERE id = ?', (id,))
     return cursor.fetchone()
 
 # Get user by username
+@queryLock
 def get_user_by_username(username):
-    cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+    execute_query('SELECT * FROM users WHERE username = ?', (username,))
     return cursor.fetchone()
 
 # Get user by username and password
+@queryLock
 def get_user_by_username_and_password(username, password):
-    cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+    execute_query('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
     return cursor.fetchone()
 
 # Insert user
+@queryLock
 def insert_user(username, password, firstname, lastname):
-    cursor.execute('INSERT INTO users (username, password, firstname, lastname) VALUES (?, ?, ?, ?)',
-                   (username, password, firstname, lastname))
-    conn.commit()
+    execute_query('INSERT INTO users (username, password, firstname, lastname) VALUES (?, ?, ?, ?)',
+                       (username, password, firstname, lastname))
 
 # Update user sid
+@queryLock
 def update_user_sid(username, sid):
-    cursor.execute('UPDATE users SET sid = ? WHERE username = ?', (sid, username))
-    conn.commit()
+    execute_query('UPDATE users SET sid = ? WHERE username = ?', (sid, username))
