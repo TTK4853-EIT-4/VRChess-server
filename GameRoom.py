d99bfb8494d78a3965f5930c2fd782e6b00a946e
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2024-03-13 17:55:29 +0100

    Reworking some of the server code

--- GameRoom.py
+++ GameRoom.py
@@ -19,7 +19,6 @@ class GameRoom:
         self.game_status = GameStatus.WAITING
         self.game_winner = None
         self.game_loser = None
-        self.read_only = False
 
     def add_opponent(self, opponent):
         # Check if there is already an opponent in the room
@@ -80,9 +79,6 @@ class GameRoom:
 
     def get_game(self):
         return self.game
-    
-    def is_read_only(self):
-        return self.read_only
     
     def serialize(self):
         return json.dumps(self, cls=GameRoomJSONEncoder)
